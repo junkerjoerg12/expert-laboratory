53248769bb75c1c7dfcb53ba23d598f1ee339a81
junkerjoerg12 <138699069+junkerjoerg12@users.noreply.github.com>
2024-02-01 19:35:30 +0100

    trying to figure out how to run app with fxml for ui

--- src/main/java/de/junkerjoerg12/ui/Main.java
+++ src/main/java/de/junkerjoerg12/ui/Main.java
@@ -1,17 +1,13 @@
 package de.junkerjoerg12.ui;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import javax.imageio.ImageIO;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {// simmilar to JFrame
@@ -23,16 +19,24 @@ public class Main extends Application {// simmilar to JFrame
   @Override
   public void start(Stage mainWindow) throws Exception {
     // Stage stage = new Stage(); // creates a new stage
-    Group group = new Group();// a rootnode
-    Scene scene = new Scene(group, Color.LIME);// simmilar to JPanel, needs a rootnode as argument
+    // Group root = new Group();// a rootnode
+    Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+    Scene scene = new Scene(root, 199, 245, Color.PINK);// simmilar to JPanel, needs a rootnode as argument
+
+    // to add a title to the window
+    // Text text = new Text("Hello World!");
+    // text.setY(50);
+    // text.setX(50);
+    // root.getChildren().add(text);
 
     // adds a logo to the window
     Image logo = new Image(getClass().getResourceAsStream("/de/junkerjoerg12/ui/ExLab.png"));
     mainWindow.getIcons().add(logo);
 
     mainWindow.setTitle("Expert Laboratory");
+    mainWindow.setMaximized(true);
     mainWindow.setScene(scene);
-    mainWindow.show();// shows the main window
+    mainWindow.show();
   }
 
 }
