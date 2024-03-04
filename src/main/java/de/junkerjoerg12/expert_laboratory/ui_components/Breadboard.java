5f46fa559b54d7e00d7fc98cd63ae24fdf6c6c4c
junkerjoerg12 <138699069+junkerjoerg12@users.noreply.github.com>
2024-03-04 21:39:23 +0100

    Connectors are horizontally and vertically drawable

--- src/main/java/de/junkerjoerg12/expert_laboratory/ui_components/Breadboard.java
+++ src/main/java/de/junkerjoerg12/expert_laboratory/ui_components/Breadboard.java
@@ -1,11 +1,19 @@
 package de.junkerjoerg12.expert_laboratory.ui_components;
 
-import de.junkerjoerg12.expert_laboratory.logicGates.LogicGate;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 
 public class Breadboard extends Pane {
+  private boolean connecting = true;
+  private boolean normal;
+
+  private Connection selectedConnection;
+  private double mouseAnchorX;
+  private double mouseAnchorY;
+
   private int gridDistance;
 
   public Breadboard(Group root, int gridDistance) {
@@ -19,6 +27,65 @@ public class Breadboard extends Pane {
     // make the color of the breadboard white
     this.setStyle("-fx-background-color: white;");
     paintGrid(gridDistance);
+
+    setOnMousePressed(new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent e) {
+        if (connecting) {
+          // create new connection
+          selectedConnection= new Connection(e.getX() - e.getX() % gridDistance, e.getY() - e.getY( )% gridDistance);
+          selectedConnection.setStyle("-fx-stroke: grey");
+          getChildren().add(selectedConnection);
+          mouseAnchorY = e.getY() - e.getY() % gridDistance;
+          mouseAnchorX = e.getX() - e.getX() % gridDistance;
+        }
+      }
+    });
+
+    setOnMouseDragged(new EventHandler<MouseEvent>() {
+      // to maybe set the Coursor to the exact position where the Connection starts:
+      //https://stackoverflow.com/questions/2941324/how-do-i-set-the-position-of-the-mouse-in-java
+
+      @Override
+      public void handle(MouseEvent e) {
+        // selectedConnection
+        if (connecting) {
+          System.out.println("MouseAncherX = " + mouseAnchorX + " MouseanchorY = " + mouseAnchorY);
+          
+          System.out.println(" X : " + Math.abs(mouseAnchorX - e.getX()) + ", Y : " + Math.abs(mouseAnchorY - e.getY()));
+
+          if (Math.abs(mouseAnchorY - e.getY()) > Math.abs(mouseAnchorX - e.getX())) {
+            System.out.println(" Y > X ");
+            selectedConnection.setEndY(e.getY() - e.getY() % gridDistance);
+            selectedConnection.setEndX(mouseAnchorX);
+          } else {
+            System.out.println(" X > Y ");
+            selectedConnection.setEndX(e.getX() - e.getX() % gridDistance);
+            selectedConnection.setEndY(mouseAnchorY);
+          }
+        }
+      }
+    });
+  
+
+    setOnMouseReleased(new EventHandler<MouseEvent>() {
+
+      @Override
+      public void handle(MouseEvent e) {
+        if (connecting) {
+          if (e.getX() - e.getX() % gridDistance == mouseAnchorX
+              && e.getY() - e.getY() % gridDistance == mouseAnchorY) {
+            getChildren().remove(selectedConnection);
+          }
+          selectedConnection.setStyle("-fx-stroke: black");
+          selectedConnection = null;
+          System.out.println(" \n");
+          mouseAnchorX = 0;
+          mouseAnchorY = 0;
+        }
+      }
+      
+    });
   }
 
   private void paintGrid(int distanceOfLines) {
--- src/main/java/de/junkerjoerg12/expert_laboratory/ui_components/Connection.java
+++ src/main/java/de/junkerjoerg12/expert_laboratory/ui_components/Connection.java
@@ -3,9 +3,18 @@ package de.junkerjoerg12.expert_laboratory.ui_components;
 import de.junkerjoerg12.expert_laboratory.logicGates.LogicGate;
 import javafx.scene.shape.Line;
 
-public class Connedtion extends Line {
+public class Connection extends Line {
   protected LogicGate input;
   protected LogicGate output;
 
   protected boolean condition;
+
+
+  
+  public Connection(double startX, double startY) {
+    setStartX(startX);	
+    setEndX(startX);
+    setStartY(startY);
+    setEndY(startY);
+  }
 }
